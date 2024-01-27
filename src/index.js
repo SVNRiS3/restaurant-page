a8f26d83644e9d6e359c21911b79f686414f0632
Chris <krzy.driwa@gmail.com>
2024-01-27 20:33:09 +0100

    Connect main-page with index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("Hello, World!");
+import { renderMainPage } from "./main-page";
+
+renderMainPage();
---
+++ src/main-page.js
@@ -0,0 +1,5 @@
+const renderMainPage = () => {
+	console.log("render page");
+};
+
+export { renderMainPage };
