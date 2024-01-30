962d8ae0c8767c8445a64fa67deb5fa553343b97
SVNRiS3 <krzy.driwa@gmail.com>
2024-01-30 15:28:29 +0100

    Add menu items

--- src/menu-page.js
+++ src/menu-page.js
@@ -10,6 +10,18 @@ const elList = [
 const sidesList = [
 	[[".menu-part", 0], "h2", "menu-header", "Sides"],
 	[[".menu-part", 0], "div", "menu-item", "Prawns"],
+	[[".menu-item", 0], "div", "menu-item-price", "$9.90"],
+	[[".menu-part", 0], "div", "menu-item", "Onion Rings"],
+	[[".menu-item", 1], "div", "menu-item-price", "$5.90"],
+	[[".menu-part", 0], "div", "menu-item", "Chips"],
+	[[".menu-item", 2], "div", "menu-item-price", "$3.90"],
+	[[".menu-part", 1], "h2", "menu-header", "Main Dish"],
+	[[".menu-part", 1], "div", "menu-item", "Beef"],
+	[[".menu-item", 3], "div", "menu-item-price", "$15.90"],
+	[[".menu-part", 1], "div", "menu-item", "Curry"],
+	[[".menu-item", 4], "div", "menu-item-price", "$23.90"],
+	[[".menu-part", 1], "div", "menu-item", "Chicken"],
+	[[".menu-item", 5], "div", "menu-item-price", "$13.90"],
 ];
 const renderMenuPage = (parentEl) => {
 	renderPage(elList, parentEl);
